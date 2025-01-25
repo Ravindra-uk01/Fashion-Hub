4c6f7bc276369365b65d017430f63634a6c10bce
Ravindra Singh Rayal <rayalravi2001@gmail.com>
2025-01-25 23:27:10 +0530

    added navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+    <div className="navbar-logo">
+      <Link to="/">FashionHub</Link>
+    </div>
+    <ul className="navbar-links">
+      <li><Link to="/shop">Shop</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/contact">Contact</Link></li>
+    </ul>
+  </nav>
   )
 }
 
