6c5645bb5911d315fdc1e28b159a4e54e12d0b38
Ravindra Singh Rayal <rayalravi2001@gmail.com>
2025-01-30 22:55:13 +0530

    added slider

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,26 +1,44 @@
 import "../styles/slider.css";
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
+import { sliderItems } from "../data";
+import { useState } from "react";
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0)
+
+  const handleSlideClick = (direction) => { 
+      if(direction === "left"){
+        setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2);
+      }
+      else{ 
+        setSlideIndex(slideIndex < 2? slideIndex+1 : 0);
+      }
+  }
+
   return (
     <div className="slider">
-      <div className="slider_arrow left">
+      <div className="slider_arrow left" onClick={()=>handleSlideClick("left")}>
         <span className="material-icons"><ArrowLeftOutlinedIcon/></span>
       </div>
-      {/* <div className="slider_container"> */}
-        {/* <div className="slider_item">
-          <div className="slider_image">
-            <img src="/hm6.jpg" alt="img1" />
+      <div className="slider_container" style={{transform:`translateX(-${slideIndex * 100}vw)`}}>
+        {sliderItems && sliderItems.length>0 && sliderItems.map((item) =>{
+          return (
+          <div className="slider_item" style={{backgroundColor:`#${item.bg}`}} key={item.id} >
+            <div className="slider_image">
+              <img src={item.img} alt="img1" />
+            </div>
+            <div className="slider_info">
+              <h1>{item.title}</h1>
+              <p>{item.desc}</p>
+              <button>SHOP NOW</button>
+            </div>
           </div>
-          <div className="slider_info">
-            <h1>WINTER SALE</h1>
-            <h2>DON'T COMPROMISE ON STYLE!</h2>
-            <button>SHOP NOW</button>
-          </div>
-        </div> */}
-      {/* </div> */}
-      <div className="slider_arrow right">
+          )
+        })
+        }
+      </div>
+      <div className="slider_arrow right" onClick={()=>handleSlideClick("right")}>
         <span className="material-icons"><ArrowRightOutlinedIcon/></span>
       </div>
     </div>
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,17 +1,17 @@
 export const sliderItems = [
     {
       id: 1,
-      img: "https://i.ibb.co/XsdmR2c/1.png",
+      img: "https://i.ibb.co/cXFnLLV/3.png",
       title: "SUMMER SALE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "f5fafd",
+      bg: "fcf1ed",
     },
     {
       id: 2,
       img: "https://i.ibb.co/DG69bQ4/2.png",
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fcf1ed",
+      bg: "f5fafd",
     },
     {
       id: 3,
