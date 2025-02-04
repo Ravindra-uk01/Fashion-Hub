967cb762c6d6e4f353f2c1eae222408a19595ee1
Ravindra Singh Rayal <rayalravi2001@gmail.com>
2025-02-05 01:50:41 +0530

    setup the backend

---
+++ backend/app.js
@@ -0,0 +1,58 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
+import {rateLimit} from 'express-rate-limit';
+import mongoSanitize from "express-mongo-sanitize";
+import xss from 'xss-clean';
+import cookieParser from "cookie-parser";
+const app = express();
+
+// import userRoutes from './routes/userRoutes.js';
+// import authRoutes from './routes/authRoutes.js';
+// import AppError from "./utils/appError.js";
+// import {globalErrorHandler} from "./controllers/errorController.js"
+
+
+
+// middleware stacks 
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit : '50kb'}));
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// Data sanitization against NoSql queries - it should always be done after above middleware , when we get data 
+app.use(mongoSanitize());  // it prevents from $gt sign or other technique of injection
+
+// Data sanitization against XSS attacks
+app.use(xss())  // it prevents from malicious html code 
+
+app.use(express.static(`../textmaster_frontend/index.html`));  // for serving static files
+
+app.use(cors({ origin: process.env.FRONTEND_URL , credentials: true}));        // enable secure cross-origin requests between web applications
+app.use(helmet());      // Set security HTTP headers
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+app.use(morgan('dev'));  // for test only 
+
+// Limit requests from same API
+const limiter = rateLimit({
+    max : 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, Please try again in an hour'
+})
+
+// routes 
+// app.use('/api/v1/users', userRoutes)
+// app.use('/api/v1/auth',limiter, authRoutes)
+
+// app.all('*', (req, res, next)=>{
+//     next(new AppError(`Can't find ${req.originalUrl} on the server!`));
+// })
+
+//Error handlling middleware
+// app.use(globalErrorHandler);
+
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('hello ');
+import http from "http";
+import app from "./app.js";
+
+const server = http.createServer(app); 
+
+const PORT = process.env.PORT || 8800;
+server.listen(PORT, ()=>{
+    console.log("Server is running on port 3000");
+})
