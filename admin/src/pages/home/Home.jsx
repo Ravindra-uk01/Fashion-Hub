48f088da5f0d83f09f9de0291c5359315ed764be
Ravindra Singh Rayal <rayalravi2001@gmail.com>
2025-02-27 22:47:24 +0530

    working on home page

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      {/* <FeaturedInfo /> */}
+      {/* <Chart data={userData} title="User Analytics" grid dataKey="Active User"/> */}
+      <div className="homeWidgets">
+        {/* <WidgetSm/>
+        <WidgetLg/> */}
+      </div>
+    </div>
   )
 }
 
