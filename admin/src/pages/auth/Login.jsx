26fc4919350e20c37816b09c610cb45e954d1457
Ravindra Singh Rayal <rayalravi2001@gmail.com>
2025-03-08 23:08:37 +0530

    working on authentication of admin side

---
+++ admin/src/pages/auth/Login.jsx
@@ -0,0 +1,116 @@
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/login.css";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { loginSchema } from "../schemas/authSchema";
+import newRequest from "../utils/newRequest";
+import { toast, ToastContainer } from "react-toastify";
+import { setProfile } from "../reducers/userReducer";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
+const Login = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+
+  // const API = import.meta.env.VITE_API;
+  const toastData = {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const onSubmit = async (data) => {
+    try {
+      setLoading(true);
+      const res = await newRequest.post(`auth/login`, data);
+      const { status, message, user, token } = res.data;
+
+      if (status === "success") {
+        toast.success(message, { ...toastData });
+        // localStorage.setItem("token", token);
+        dispatch(setProfile({ user }));
+      } else {
+        toast.warn(message, { ...toastData });
+      }
+    } catch (err) {
+      const { status, message } = err.response.data;
+      if (status === "warning") {
+        toast.warn(message, {
+          ...toastData,
+        });
+      } else {
+        toast.error(message, {
+          ...toastData,
+        });
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="login_container">
+      <ToastContainer />
+      <div className="login">
+        <span className="loginTitle">SIGN IN</span>
+        <form className="loginForm" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <input
+              type="text"
+              className="loginInput"
+              placeholder="Email*"
+              {...register("email")}
+            />
+            <p className="err_msg">{errors.email?.message}</p>
+          </div>
+
+          <div className="loginFormDiv">
+            <input
+              type={isPasswordVisible ? "text" : "password"}
+              className="loginInput"
+              placeholder="Password*"
+              {...register("password")}
+            />
+            <button
+              type="button"
+              onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+              className="toggle-password"
+            >
+              {isPasswordVisible ? <VisibilityOffIcon /> : <VisibilityIcon />}
+            </button>
+          </div>
+          <p className="err_msg">{errors.password?.message}</p>
+
+          <button className="loginButton" type="submit" disabled={loading}>
+            Login
+          </button>
+        </form>
+        <div className="login_other_links">
+          <Link to={"/forgot_password"}>
+            DO NOT YOU REMEMBER THE PASSWORD? <br />
+          </Link>
+          <Link to={"/register"}>CREATE A NEW ACCOUNT</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ admin/src/pages/auth/Register.jsx
@@ -0,0 +1,149 @@
+import { useState } from "react";
+import "../styles/register.css";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { registrationSchema } from "../schemas/authSchema";
+import { addUser } from "../reducers/userReducer";
+import { ToastContainer } from "react-toastify";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+
+const Register = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] =
+    useState(false);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registrationSchema),
+  });
+
+  const onSubmit = async (data, e) => {
+    console.log(data);
+    e.preventDefault();
+    try {
+      setLoading(true);
+      dispatch(addUser(data));
+      setLoading(false);
+      console.log("user added successfully. ");
+    } catch (error) {
+      setLoading(false);
+      console.error("Failed to add user:", error);
+    }
+  };
+
+  return (
+    <div className="register_container">
+      <ToastContainer />
+      <div className="register">
+        <span className="registerTitle">CREATE AN ACCOUNT</span>
+        <form className="registerForm" onSubmit={handleSubmit(onSubmit)}>
+          <div className="registerFormDiv">
+            <input
+              type="text"
+              className="registerInput"
+              placeholder="first name*"
+              {...register("first_name")}
+            />
+            <p className="err_msg">{errors.first_name?.message}</p>
+          </div>
+
+          <div className="registerFormDiv">
+            <input
+              type="text"
+              className="registerInput"
+              placeholder="last name "
+              {...register("last_name")}
+            />
+          </div>
+          <div className="registerFormDiv">
+            <input
+              type="text"
+              className="registerInput"
+              placeholder="Username"
+              {...register("username")}
+            />
+          </div>
+          <div className="registerFormDiv">
+            <input
+              type="text"
+              className="registerInput"
+              placeholder="Email*"
+              {...register("email")}
+            />
+            <p className="err_msg">{errors.email?.message}</p>
+          </div>
+
+          <div className="registerFormDiv">
+            <div className="registerPasswordDiv">
+              <input
+                type={isPasswordVisible ? "text" : "password"}
+                className="registerInput"
+                placeholder="Password*"
+                {...register("password")}
+              />
+              <button
+                type="button"
+                onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                className="toggle-password reg"
+              >
+                {isPasswordVisible ? (
+                  <VisibilityOffIcon fontSize="small" />
+                ) : (
+                  <VisibilityIcon fontSize="small" />
+                )}
+              </button>
+            </div>
+            <p className="err_msg">{errors.password?.message}</p>
+          </div>
+
+          <div className="registerFormDiv">
+            <div className="registerPasswordDiv">
+              <input
+                type={isConfirmPasswordVisible ? "text" : "password"}
+                className="registerInput"
+                placeholder="Confirm Password*"
+                {...register("confirm_password")}
+              />
+              <button
+                type="button"
+                onClick={() =>
+                  setIsConfirmPasswordVisible(!isConfirmPasswordVisible)
+                }
+                className="toggle-password reg"
+              >
+                {isConfirmPasswordVisible ? (
+                  <VisibilityOffIcon fontSize="small" />
+                ) : (
+                  <VisibilityIcon fontSize="small" />
+                )}
+              </button>
+            </div>
+            <p className="err_msg">{errors.confirm_password?.message}</p>
+          </div>
+          <div className="registerAggrement">
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY.</b>
+          </div>
+          <button className="registerButton" type="submit" disabled={loading}>
+            Register
+          </button>
+        </form>
+        <div className="register_links">
+          <Link to={"/login"}>
+            Already Have an Account? <br />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
